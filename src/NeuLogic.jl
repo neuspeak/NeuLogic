74ff2eb150041244c31c168db614b2a2de871997
ComplYue <compl.yue@icloud.com>
2023-12-28 22:30:56 +0800

    c1bnet

--- src/NeuLogic.jl
+++ src/NeuLogic.jl
@@ -6,4 +6,8 @@ include("./bint8.jl")
 using .BInt8Type
 
 
+include("./c1bnet.jl")
+using .C1BNet
+
+
 end # module NeuLogic
---
+++ src/c1bnet.jl
@@ -0,0 +1,12 @@
+
+module C1BNet
+
+# Neural Network to do Formal Logic
+#
+# using a model inspired by a single Cortical Column in brain
+
+
+using ..BInt8Type
+
+
+end # module C1BNet 
